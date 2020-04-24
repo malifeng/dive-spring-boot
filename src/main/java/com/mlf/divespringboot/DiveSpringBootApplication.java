ad205061e4f069daa9f412d3a3be2f1f5ac36930
malifeng <1005514873@qq.com>
2020-04-24 12:31:25 +0800

    init

---
+++ src/main/java/com/mlf/divespringboot/DiveSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mlf.divespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiveSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlf/divespringboot/DiveSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlf.divespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
