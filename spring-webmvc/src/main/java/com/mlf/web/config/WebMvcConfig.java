25265f79197095bf7a95b0143d1afba6f960d4c6
malifeng <1005514873@qq.com>
2020-04-28 15:22:02 +0800

    spring mvc

---
+++ spring-webmvc/src/main/java/com/mlf/web/config/WebMvcConfig.java
@@ -0,0 +1,53 @@
+package com.mlf.web.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//<!--    <bean class="org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping"/>-->
+//<!--    <bean class="org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter"/>-->
+//<!--    <bean id="viewResolver" class="org.springframework.web.servlet.view.InternalResourceViewResolver">-->
+//<!--        <property name="viewClass" value="org.springframework.web.servlet.view.JstlView"/>-->
+//<!--        <property name="prefix" value="/WEB-INF/jsp/"/>-->
+//<!--        <property name="suffix" value=".jsp"/>-->
+//<!--    </bean>-->
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.out.println("addInterceptors...");
+                return true;
+            }
+        });
+    }
+}
--- spring-webmvc/src/main/java/com/mlf/web/controller/HelloWorlController.java
+++ spring-webmvc/src/main/java/com/mlf/web/controller/HelloWorlController.java
@@ -11,3 +11,4 @@ public class HelloWorlController {
         return "index";
     }
 }
+
